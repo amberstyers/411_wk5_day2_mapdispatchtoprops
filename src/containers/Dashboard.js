4a5da1bdf92e7be96d811245f8a2db29fa5feb55
amberstyers <amberstyers@gmail.com>
2020-02-26 20:35:34 -0600

    added dispatchtoprops to Dashboard Container, not working yet

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux'
 import Dashboard from '../components/Dashboard'
+import { removeCar } from '../'
 // import { removeCar } action here
 
 const mapStateToProps = (state) => {
@@ -10,5 +11,11 @@ const mapStateToProps = (state) => {
 }
 
 // add mapDispatchToProps function here
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addCar: (car) => dispatch(addCar(car)),
+        removeCar: (index) => dispatch(removeCar(index))
+    }
+}
 
 export default connect(mapStateToProps)(Dashboard)
