587bae988db3713a2e0d884e72f865ba973155b3
amberstyers <amberstyers@gmail.com>
2020-03-02 20:35:33 -0600

    says params undefined in car.js component

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux'
 import Dashboard from '../components/Dashboard'
 import { removeCar, addCar } from '../redux/action'
+// I deleted the one above to see if just removeCar would make this sheeeeet work
+// import { removeCar } from '../redux/action'
 // import { removeCar } action here
 
 const mapStateToProps = (state) => {
